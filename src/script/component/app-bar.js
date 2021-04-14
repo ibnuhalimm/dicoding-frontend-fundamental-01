94f6fa05cc7cab0243ec560c3845bc6fec2abd6d
Ibnu Halim Mustofa <ibnuhalimm@gmail.com>
2021-04-14 12:55:00 +0700

    apply shadow dom to app-bar component

--- src/script/component/app-bar.js
+++ src/script/component/app-bar.js
@@ -1,10 +1,34 @@
 class AppBar extends HTMLElement {
+    constructor() {
+        super();
+        this._shadowDom = this.attachShadow({ mode: 'open' });
+    }
+
     connectedCallback() {
         this.render();
     }
 
     render() {
-        this.innerHTML = `<h2>Club Finder</h2>`;
+        this._shadowDom.innerHTML = `
+            <style>
+                * {
+                    margin: 0px;
+                    padding: 0px;
+                    box-sizing: border-box;
+                }
+                :host {
+                    display: block;
+                    width: 100%;
+                    background-color: cornflowerblue;
+                    color: white;
+                    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+                }
+                h2 {
+                    padding: 16px;
+                }
+            </style>
+            <h2>Club Finder</h2>
+        `;
     }
 }
 
