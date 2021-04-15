33d778d80a955071af99443db4400709df2b86f1
Ibnu Halim Mustofa <ibnuhalimm@gmail.com>
2021-04-15 19:51:10 +0700

    get data from thesportsdb API

--- src/script/view/main.js
+++ src/script/view/main.js
@@ -8,8 +8,11 @@ const main = () => {
 
     const onButtonSearchClicked = async () => {
         try {
-            const result = await DataSource.searchClub(searchElement.value);
-            renderResult(result);
+            const keyword = searchElement.value;
+            const response = await fetch(`https://www.thesportsdb.com/api/v1/json/1/searchteams.php?t=${keyword}`);
+            const responseJson = await response.json();
+
+            renderResult(responseJson.teams);
 
         } catch (error) {
             fallbackResult(error);
